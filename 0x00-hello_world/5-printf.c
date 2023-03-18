714472f820c5631cf9784694435a8598e2e389bc
jeremiah oluwaditire <sugfedcofor@gmail.com>
2023-03-18 09:48:37 -0700

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,11 @@
 #include <stdio>
+/**
+ * main - A progam that prints a line with the printf function
+ * Return: Always (0) success
+ */
+int main(void)
+{
+pintf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
 
