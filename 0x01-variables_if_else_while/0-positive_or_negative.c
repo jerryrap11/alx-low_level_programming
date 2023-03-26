c0be89644951fd7ca731806367e27883f86cd58a
jeremiah oluwaditire <sugfedcofor@gmail.com>
2023-03-26 06:02:06 -0700

    nature made the natural numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/** Main - A program that prints a line with print f **/
+/* betty style doc for function main goes there */
+/**
+* Main - A program that prints a line with print f
+* Return: Always 0  for success
+**/
 int Main(void)
 {
 	int n;
@@ -11,10 +15,11 @@ int Main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 if (n > 0)
-print f(" % d is positive", n);
+printf("%d is positive", n);
 else if (n == 0)
-print f(" % d is zero", n);
-else if (n < 0)
-print f(" % d is negative", n);
-	return (0);
+printf("%d is zero", n);
+else
+printf("%d is negative", n);
+printf("\n")
+return 0;
 }
